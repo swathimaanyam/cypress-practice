4b5cbb760df248358c6afac013046c0290bf0982
Swathi Maanyam <swathi@Swathis-MacBook-Air.local>
2025-02-10 21:52:43 -0800

    fix covered element issue

--- cypress/e2e/pages/loginPage.js
+++ cypress/e2e/pages/loginPage.js
@@ -8,8 +8,7 @@ class loginPage{
     login(email,password){
         this.elements.email().type(email);
         this.elements.password().type(password, {log : false});
-        this.elements.submitButton().click()
-
+        this.elements.submitButton().should('not.be.disabled').click()
     }
 }
 module.exports= new loginPage();
