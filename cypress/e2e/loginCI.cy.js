4985f7ec2edfd0a57d118ff7a1cfeba5e00f1012
Swathi Maanyam <swathi@Swathis-MacBook-Air.local>
2025-02-11 13:33:10 -0800

    added login steps to spec file instead of pom

--- cypress/e2e/loginCI.cy.js
+++ cypress/e2e/loginCI.cy.js
@@ -5,6 +5,9 @@ describe('login functionality', () => {
         cy.visit("/users/log_in")
     })
     it('valid login with ENV variables', () => {
-        loginPage.login(Cypress.env('CYPRESS_VALID_USERNAME'), Cypress.env('CYPRESS_VALID_PASSWORD'));
+        // loginPage.login(Cypress.env('CYPRESS_VALID_USERNAME'), Cypress.env('CYPRESS_VALID_PASSWORD'));
+        cy.get("#user_email").type(Cypress.env('CYPRESS_VALID_USERNAME'));
+        cy.get("#password-input").type(Cypress.env('CYPRESS_VALID_PASSWORD'));
+        cy.get(".btn-primary ").click()
     })
 })
