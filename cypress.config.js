878a4aca5732e2233be73141b96831a8f19aa1e6
Swathi Maanyam <swathi@Swathis-MacBook-Air.local>
2025-02-11 16:19:43 -0800

    changes

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://nobsbusinessmembership.com",
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
   },
 });
